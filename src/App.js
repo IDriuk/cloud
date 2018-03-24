28abb52cfb6168383e2a7052e1429e2f2e2427ea
Ivan Driuk <veytarn@gmail.com>
2018-03-24 20:22:38 +0200

    add development api server url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const api = 'https://corsback.herokuapp.com/';
+import { api } from './config';
 
 class App extends Component {
 
