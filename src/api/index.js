1d400a77666ee3fa9ecd60d5008920d0a90fd919
Ivan Driuk <veytarn@gmail.com>
2018-03-25 17:24:28 +0300

    add redux, move App component to containers, move fetch request to api

---
+++ src/api/index.js
@@ -0,0 +1,17 @@
+const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
+const apiUrl = isLocalhost ? 'http://localhost:3001/'
+                           : 'https://corsback.herokuapp.com/';
+
+export default () => fetch(apiUrl)
+                       .then((response) => { return response.text(); })
+                       .then((text) => console.log(text))
+                       .catch(e => console.log('request failed'));
--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
 import './index.css';
-import App from './App';
+import App from './containers/App/App';
+import api from './api';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+api();
+
+const store = createStore(
+  () => {},
+  applyMiddleware(thunk)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
