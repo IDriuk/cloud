2f566ea8eafeffef3771b125e86c60e17dde84be
Ivan Driuk <veytarn@gmail.com>
2018-03-26 15:46:51 +0300

    return data in global state with reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,12 @@
-export default () => {}
+import * as types from '../constants/ActionTypes'
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case types.RECEIVE_ALL:
+      return {
+        data: action.data
+      }
+    default:
+      return state
+  }
+}
