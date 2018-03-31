5520ebcae8349cbf7fc8c19a50f7199b74e56c14
Ivan Driuk <veytarn@gmail.com>
2018-03-31 08:19:51 +0300

    make Chart component from function

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,29 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './Chart.css';
 
-export default ({ data }) => {
+class Chart extends Component {
 
-  if (!data) return <div> Loading... </div>
+  render() {
+    const { data } = this.props;
 
-  const { coordinates } = data;
+    if (!data) return <div> Loading... </div>
 
-  let M = coordinates.splice(0, 1)[0];
-  const d = coordinates.reduce((acc, {x, y}) => `${acc}L${x},${y}`, `M${M.x},${M.y}` );
+    const { coordinates } = data;
 
-  const { x, y } = coordinates[222];
+    let M = coordinates.splice(0, 1)[0];
+    const d = coordinates.reduce((acc, {x, y}) => `${acc}L${x},${y}`, `M${M.x},${M.y}` );
 
-  return (
-    <div>
-      <svg xmlns="http://www.w3.org/2000/svg" width="1024" height="200">
-        <g >
-            <g >
-              <path fill="none" strokeWidth="1" stroke="#4D90FE" d={d} >
-              </path>
-            </g>
-            <g>
-              <circle cx={x} cy={y} fill="#4D90FE" r={4} ></circle>
-            </g>
-        </g>
-      </svg>
-    </div>
-  );
+    const { x, y } = coordinates[222];
+
+    return (
+      <div className="Chart">
+        <svg xmlns="http://www.w3.org/2000/svg" width="1024" height="200">
+          <g >
+              <g >
+                <path fill="none" strokeWidth="1" stroke="#4D90FE" d={d} >
+                </path>
+              </g>
+              <g>
+                <circle cx={x} cy={y} fill="#4D90FE" r={4} ></circle>
+              </g>
+          </g>
+        </svg>
+      </div>
+    );
+  }
 }
+
+export default Chart;
