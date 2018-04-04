7ad64688eb7ba0911cdf4d38c60becbce416bd30
Ivan Driuk <veytarn@gmail.com>
2018-04-04 07:35:58 +0300

    create dumb components for icons and logos, save user photo to assets

---
+++ src/assets/icons/project.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+export default () =>
+<svg id="project" width="100%" height="100%" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" fit="" preserveAspectRatio="xMidYMid meet" focusable="false"><path d="M10.557 11.99l-1.71-2.966 1.71-3.015h3.42l1.71 3.01-1.71 2.964h-3.42zM4.023 16l-1.71-2.966 1.71-3.015h3.42l1.71 3.01L7.443 16h-3.42zm0-8.016l-1.71-2.966 1.71-3.015h3.42l1.71 3.015-1.71 2.966h-3.42z" fill-rule="evenodd"></path></svg>
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,24 @@ import './App.css';
 
 // import ChartContainer from '../../containers/ChartContainer';
 
+// icons
+import MenuIcon from '../../assets/icons/menu';
+import ProjectIcon from '../../assets/icons/project';
+import SearchIcon from '../../assets/icons/search';
+import GiftIcon from '../../assets/icons/gift';
+import DevShellIcon from '../../assets/icons/dev_shell';
+import FeedBackIcon from '../../assets/icons/feedback';
+import HelpIcon from '../../assets/icons/help';
+import NotificationsIcon from '../../assets/icons/notifications';
+import SettingsIcon from '../../assets/icons/settings';
+
+// logos
+import GoogleLogo from '../../assets/logos/google_logo.js';
+import CloudConsoleLogo from '../../assets/logos/cloud_console_logo.js';
+
+// images
+import userPhoto from '../../assets/images/user_photo.jpg';
+
 class App extends Component {
 
   render() {
@@ -14,20 +32,18 @@ class App extends Component {
             <div className="p6n-platformbar-container p6n-prober-logo">
               <div className="p6n-platformbar-left">
                 <button className="p6n-platformbar-colored-icon-container p6n-platformbar-button md-primary md-button md-platform-bar-theme md-ink-ripple md-icon-button">
-                  <svg id="menu" width="100%" height="100%" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fit="" preserveAspectRatio="xMidYMid meet" focusable="false">
-                    <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" fill-rule="evenodd"></path>
-                  </svg>
+                  <MenuIcon />
                 </button>
                 <a className="p6n-product-logo md-platform-bar-theme">
                   <div className="p6n-logo p6n-logo-theme-light">
-                    <svg width="100%" height="100%" viewBox="0 0 72 24" xmlns="http://www.w3.org/2000/svg" fit="" preserveAspectRatio="xMidYMid meet" focusable="false"> <path xmlns="http://www.w3.org/2000/svg" d="M16 13l4.578-.11c-.185 1.204-.556 2.038-1.112 2.594-.74.74-1.853 1.483-3.8 1.483-3.057 0-5.467-2.502-5.467-5.56 0-3.06 2.41-5.56 5.466-5.56 1.668 0 2.873.648 3.707 1.482l1.482-1.3C19.56 4.827 17.89 3.9 15.575 3.9c-4.17 0-7.693 3.43-7.693 7.6s3.522 7.598 7.692 7.598c2.224 0 3.985-.74 5.282-2.13 1.39-1.39 1.76-3.338 1.76-4.913 0-.463-.016-.556-.016-1.055H16v2zm12.92-3.818c-2.69 0-4.913 2.04-4.913 4.912 0 2.873 2.224 4.91 4.912 4.91 2.685 0 4.91-2.037 4.91-4.91.092-2.873-2.13-4.912-4.91-4.912zm0 7.877c-1.484 0-2.78-1.206-2.78-2.967 0-1.76 1.296-2.966 2.78-2.966 1.482 0 2.78 1.205 2.78 2.966 0 1.76-1.298 2.965-2.78 2.965zm21.498-7.97c-2.41 0-4.633 2.13-4.633 4.91 0 2.78 2.13 4.912 4.633 4.912 1.205 0 2.132-.556 2.595-1.112h.093v.65c0 1.853-1.02 2.872-2.595 2.872-1.295 0-2.13-.927-2.5-1.76l-1.853.74c.556 1.298 1.946 2.874 4.355 2.874 2.503 0 4.49-1.357 4.49-5.097V9.366h-1.894v.834a3.814 3.814 0 0 0-2.688-1.11zm.278 7.97c-1.482 0-2.594-1.3-2.594-2.967 0-1.76 1.112-2.966 2.594-2.966 1.483 0 2.595 1.298 2.595 2.966 0 1.668-1.11 2.965-2.593 2.965zm-10.75-7.88c-2.687 0-4.91 2.04-4.91 4.912 0 2.873 2.223 4.91 4.91 4.91 2.688 0 4.912-2.037 4.912-4.91s-2.224-4.912-4.91-4.912zm0 7.877c-1.482 0-2.78-1.205-2.78-2.966 0-1.76 1.298-2.965 2.78-2.965 1.483 0 2.78 1.205 2.78 2.966 0 1.76-1.297 2.966-2.78 2.966zM57 4.084h2v14.828h-2V4.085zm8.616 12.974c-1.112 0-1.853-.464-2.41-1.483l6.58-2.688-.277-.65c-.373-1.112-1.67-3.15-4.173-3.15s-4.633 1.946-4.633 4.91c0 2.78 2.04 4.913 4.91 4.913 2.226 0 3.523-1.39 4.08-2.132l-1.67-1.11c-.555.833-1.39 1.39-2.41 1.39zm-.185-6.117c.836 0 1.578.464 1.856 1.113l-4.45 1.853c0-2.038 1.484-2.965 2.596-2.965z" fill-rule="evenodd"></path> </svg>
-                    <svg width="100%" height="100%" viewBox="0 0 130 24" xmlns="http://www.w3.org/2000/svg" fit="" preserveAspectRatio="xMidYMid meet" focusable="false"> <path xmlns="http://www.w3.org/2000/svg" d="M9.176 19.283a7.324 7.324 0 0 0 5.56-2.502L13.53 15.58c-1.017 1.204-2.5 2.038-4.26 2.038-2.966 0-5.376-2.13-5.376-5.56 0-3.43 2.503-5.56 5.375-5.56 1.575 0 2.872.556 3.89 1.76l1.206-1.204c-1.206-1.482-3.06-2.317-5.098-2.317-4.077 0-7.228 3.15-7.228 7.32s3.058 7.23 7.136 7.23zm8.685-.278V5.012H16.1v13.993h1.76zm22.08 0V9.46h-1.92v5.286c0 1.483-1.01 2.954-2.52 2.954-1.345 0-2.5-.427-2.5-2.68V9.46h-2v5.84c0 2.315 1.733 3.988 4.027 3.988 1.235 0 2.797-.746 3.326-1.488h.088v1.113h1.5v.092zm5.85.278c1.484 0 2.69-.74 3.245-1.668h.092v1.298h1.76v-13.9h-1.76V9.46l.093 1.298h-.094c-.556-.834-1.76-1.668-3.243-1.668-2.41 0-4.54 2.13-4.54 5.097 0 2.965 2.038 5.096 4.447 5.096zm.28-1.668c-1.576 0-3.06-1.297-3.06-3.428 0-2.132 1.484-3.43 3.06-3.43 1.575 0 3.057 1.298 3.057 3.43 0 2.13-1.482 3.428-3.058 3.428zM59.87 13h2.966c2.367.05 4.356-1.668 4.356-4.02 0-2.352-2.04-3.968-4.356-3.968H58.11v13.993h1.76V13zm.1-1.83v-4.3h2.91c1.55 0 2.46 1.158 2.46 2.15s-.91 2.15-2.46 2.15h-2.91zm10.89 7.835V5.012H69v13.993h1.86zm8.204-1.39h.092v1.298h1.76v-5.746c0-2.687-1.945-4.077-4.262-4.077-2.41 0-3.52 1.482-3.892 2.316l1.668.742c.37-.927 1.298-1.39 2.317-1.39 1.39 0 2.502.834 2.502 2.316v.278c-.373-.185-1.3-.556-2.597-.556-2.224 0-3.892 1.14-3.892 3.273-.184 1.944 1.206 2.98 3.15 2.98 1.67 0 2.597-.602 3.153-1.436zm-4.624-1.598c0-.834.393-1.67 2.247-1.67 1.482 0 2.372.58 2.372.58 0 1.484-1.577 2.537-2.967 2.537-.927 0-1.654-.428-1.654-1.447zm14.072 2.803l-.65-1.575c-.277.092-.462.185-.833.185-.836 0-1.3-.463-1.13-1.575V11H88V9.46h-2.1l.13-2.436H84l-.03 2.436H82V11h2v5.133c0 2.008.99 2.965 3.03 2.965 0 0 1.018-.093 1.482-.278zM91 8.102V9.46h-1.892v1.56H91.1v7.985h1.86V11.02H96V9.46h-3.04V8.256c0-1.112.74-1.67 1.575-1.67.37 0 .556 0 .834.187l.647-1.576c-.37-.185-.834-.278-1.39-.278-2.04 0-3.53 1.235-3.628 3.18zM102 9c-2.925 0-5 2.182-5 5s2.075 5 5 5 5-2.182 5-5-2.075-5-5-5zM25 9c-2.925 0-5 2.182-5 5s2.075 5 5 5 5-2.182 5-5-2.075-5-5-5zm77 8.4c-1.5 0-3-1.287-3-3.4 0-2.114 1.412-3.4 3-3.4 1.5 0 3 1.286 3 3.4 0 2.113-1.5 3.4-3 3.4zm-77 0c-1.5 0-3-1.287-3-3.4 0-2.114 1.412-3.4 3-3.4 1.5 0 3 1.286 3 3.4 0 2.113-1.5 3.4-3 3.4zm84.946 1.605v-5.19c0-1.76 1.205-2.78 2.502-2.78.37 0 .742 0 .927.093l.65-1.668c-.372-.185-.743-.278-1.39-.278-1.02 0-2.41.742-2.782 1.76h-.092V9.46H108v9.545h1.946zm7.014 0v-5.282c-.2-1.483.82-2.965 2.21-2.965 1.39 0 2.132.926 1.83 2.965l.06 5.282H123v-5.282c.062-1.483.804-2.965 2.194-2.965s1.826.68 1.826 2.718v5.53l1.982-.006v-5.85c0-2.317-.982-3.968-3.3-3.968-1.482 0-2.454.742-3.103 1.854-.465-1.112-1.485-1.854-3.06-1.854-1.11 0-2.41.742-2.966 1.668V9.46H115v9.545h1.96z" fill-rule="evenodd"></path> </svg>
+                    <GoogleLogo />
+                    <CloudConsoleLogo />
                   </div>
                 </a>
                 <div className="p6n-color-white p6n-project-switcher p6n-picker-wrapper">
                   <div className="p6n-dropdown-menu">
                     <a className="goog-flat-menu-button md-platform-bar-theme">
-                      <svg id="project" width="100%" height="100%" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" fit="" preserveAspectRatio="xMidYMid meet" focusable="false"><path d="M10.557 11.99l-1.71-2.966 1.71-3.015h3.42l1.71 3.01-1.71 2.964h-3.42zM4.023 16l-1.71-2.966 1.71-3.015h3.42l1.71 3.01L7.443 16h-3.42zm0-8.016l-1.71-2.966 1.71-3.015h3.42l1.71 3.015-1.71 2.966h-3.42z" fill-rule="evenodd"></path></svg>
+                      <ProjectIcon />
                       <span className="p6n-label p6n-project-switcher-project-name">idriukreact</span>
                     </a>
                   </div>
@@ -37,7 +53,7 @@ class App extends Component {
                 <div className="p6n-search-container">
                   <div className="p6n-search-bar p6n-color-white">
                     <span className="p6n-platformbar-utility">
-                      <svg id="search" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" fit="" height="100%" width="100%" preserveAspectRatio="xMidYMid meet" focusable="false"> <g stroke="none" stroke-width="1" fill-rule="evenodd"> <path d="M11.1788625,9.74662961 C11.6979503,8.95839217 12,8.01451178 12,7 C12,4.23829787 9.76170213,2 7,2 C4.23829787,2 2,4.23829787 2,7 C2,9.76170213 4.23829787,12 7,12 C8.02383003,12 8.97572521,11.6923762 9.76830994,11.1645042 L14.267767,15.6639612 L15.6819805,14.2497477 L11.1788625,9.74662961 Z M7,10 C8.65685425,10 10,8.65685425 10,7 C10,5.34314575 8.65685425,4 7,4 C5.34314575,4 4,5.34314575 4,7 C4,8.65685425 5.34314575,10 7,10 Z"></path> </g> </svg>
+                      <SearchIcon />
                     </span>
                     <input className="p6n-search-input"></input>
                   </div>
@@ -46,28 +62,28 @@ class App extends Component {
               <div className="p6n-platformbar-right">
                 <div className="p6n-platformbar-left">
                   <button className="p6n-platformbar-colored-icon-container p6n-platformbar-button md-primary md-button md-platform-bar-theme md-ink-ripple md-icon-button">
-                    <svg id="gift" height="100%" width="100%" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fit="" preserveAspectRatio="xMidYMid meet" focusable="false"> <g stroke="none" stroke-width="1" fill-rule="evenodd"> <path d="M11 21H5.75C4.784 21 4 20.283 4 19.4V13h7v8zm9-1.6c0 .883-.784 1.6-1.75 1.6H13v-8h7v6.4zM11 7v5H3V8.667C3 7.747 3.717 7 4.6 7H11zm8.4 0c.883 0 1.6.747 1.6 1.667V12h-8V7h6.4zm-2.204-1H6.804c-2.922 0-3.22-3 0-3C9.14 3 10.11 3.666 12 4.492 14.15 3.666 14.86 3 17.196 3c3.22 0 2.922 3 0 3z" id="shape"></path> </g> </svg>
+                    <GiftIcon />
                   </button>
                   <button className="p6n-platformbar-colored-icon-container p6n-platformbar-button md-primary md-button md-platform-bar-theme md-ink-ripple md-icon-button">
-                    <svg id="devshell" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" fit="" height="100%" width="100%" preserveAspectRatio="xMidYMid meet" focusable="false"> <g> <path d="M9,10 L6,7 L3,7 L6,10 L3,13 L6,13 L9,10 Z M14,12 L14,11 L10,11 L10,12 L10,13 L14,13 L14,12 Z M2,4.00585866 C2,2.89805351 2.89706013,2 4.00585866,2 L13.9941413,2 C15.1019465,2 16,2.89706013 16,4.00585866 L16,13.9941413 C16,15.1019465 15.1029399,16 13.9941413,16 L4.00585866,16 C2.89805351,16 2,15.1029399 2,13.9941413 L2,4.00585866 Z"></path> </g> </svg>
+                    <DevShellIcon />
                   </button>
                   <button className="p6n-platformbar-colored-icon-container p6n-platformbar-button md-primary md-button md-platform-bar-theme md-ink-ripple md-icon-button">
-                    <svg id="feedback_cache10" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" fit="" height="100%" width="100%" preserveAspectRatio="xMidYMid meet" focusable="false"> <g stroke="none" stroke-width="1" fill-rule="evenodd"> <path d="M2.36334387,12.9099684 C1.502539,11.7935074 1,10.448105 1,9 C1,5.13400675 4.581722,2 9,2 C13.418278,2 17,5.13400675 17,9 C17,12.8659932 13.418278,16 9,16 C7.85276228,16 6.76192672,15.7886994 5.77555178,15.4081494 L1,17 L2.36334387,12.9099684 Z M10,13.5 L10,14 L8,14 L8,12.5 L8,12 L10,12 L10,13.5 Z M9.875,11 L8.125,11 L8.00560003,8.9787775 L8.00113325,5.00752992 C8.00050737,4.45108651 8.44386482,4 9,4 C9.55228475,4 10,4.44371665 10,4.99980749 L10,9 L9.875,11 Z"></path> </g> </svg>
+                    <FeedBackIcon />
                   </button>
                   <button className="p6n-platformbar-colored-icon-container p6n-platformbar-button md-primary md-button md-platform-bar-theme md-ink-ripple md-icon-button">
-                    <svg id="help" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" fit="" height="100%" width="100%" preserveAspectRatio="xMidYMid meet" focusable="false"> <g> <path d="M9,16 C12.8659932,16 16,12.8659932 16,9 C16,5.13400675 12.8659932,2 9,2 C5.13400675,2 2,5.13400675 2,9 C2,12.8659932 5.13400675,16 9,16 Z M11.471463,8.41263722 C11.3109097,8.62427134 11.1280898,8.82722303 10.9218315,9.0225776 C10.7155732,9.21793218 10.5034552,9.41003084 10.2831339,9.6010442 C10.0311706,9.81376362 9.86358567,9.96896198 9.77920727,10.2207523 C9.69482886,10.4725427 9.65029582,11 9.64560813,11 L8.08695149,11 C8.08695149,11 8.10687416,10.5137842 8.1455476,10.291297 C8.18422104,10.0688099 8.24750484,9.90384379 8.33657093,9.71934225 C8.42563703,9.53484071 8.54165733,9.37964235 8.68345993,9.21359096 C8.82526253,9.04753958 8.99987895,8.88148819 9.20496535,8.69481604 C9.36083101,8.55481193 9.50614937,8.42674616 9.64092044,8.30193629 C9.7756915,8.17712643 9.89522757,8.05123126 9.99835673,7.91990957 C10.1014859,7.78858789 10.1823485,7.64966908 10.2421166,7.50098254 C10.3018846,7.35229601 10.3311827,7.18732992 10.3311827,7.00608429 C10.3311827,6.60777803 10.2245377,6.30389313 10.0124198,6.09334432 C9.80030187,5.8827955 9.48739862,5.7796917 9.07488197,5.7796917 C8.90964093,5.7796917 8.1455476,5.73845024 7.87600547,6.27024874 C7.79983053,6.41459406 7.68765855,6.75728196 7.68765854,6.94856329 L6,6.94856329 C6.07160502,6.35462418 6.17789008,6.04233507 6.3314119,5.73845018 C6.48493372,5.43456529 6.69587973,5.24789314 6.96307801,5.04277084 C7.23027629,4.83764854 7.54435147,4.71500928 7.90413161,4.61190548 C8.26391175,4.50880167 8.65416187,4.47190136 9.07488197,4.47190136 C9.53779128,4.47190136 9.949136,4.53484895 10.31126,4.64554987 C10.673384,4.7562508 10.9792557,4.92121688 11.231219,5.132851 C11.4831823,5.34448512 11.6742056,5.60604319 11.804289,5.91101338 C11.9343724,6.21598358 12,6.5632806 12,6.94856322 C12,7.22857145 11.9519512,7.49012951 11.8558536,7.73215212 C11.7597559,7.97417473 11.6320164,8.2010031 11.471463,8.41263722 Z M8.99627792,14 C8.67617866,14 8.43052109,13.9058824 8.25806452,13.7163399 C8.08560794,13.5267974 8,13.2928105 8,13.0117647 C8,12.869281 8.02233251,12.7359477 8.06575682,12.6130719 C8.10918114,12.4901961 8.17369727,12.3830065 8.25806452,12.2915033 C8.34243176,12.2 8.44665012,12.1281046 8.5707196,12.0771242 C8.69478908,12.0261438 8.83622829,12 8.99627792,12 C9.15632754,12 9.29776675,12.0261438 9.42183623,12.0771242 C9.54590571,12.1281046 9.65012407,12.2 9.73449132,12.2915033 C9.81885856,12.3830065 9.88461538,12.4901961 9.93052109,12.6130719 C9.9764268,12.7359477 10,12.8679739 10,13.0117647 C10,13.2928105 9.91315136,13.5281046 9.7382134,13.7163399 C9.56327543,13.9058824 9.31637717,14 8.99627792,14 Z"></path> </g> </svg>
+                    <HelpIcon />
                   </button>
                   <button className="p6n-platformbar-colored-icon-container p6n-platformbar-button md-primary md-button md-platform-bar-theme md-ink-ripple md-icon-button">
-                    <svg id="notifications" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" fit="" height="100%" width="100%" preserveAspectRatio="xMidYMid meet" focusable="false"> <g> <path d="M10.5,15 L7.5,15 C7.5,16.1 8.175,17 9,17 C9.825,17 10.5,16.1 10.5,15 Z M9.99515756,3.09903766 C12.2798101,3.56043498 14,5.57933155 14,8 L14,11.9957886 L15,12.9957886 L15,14 L3,14 L3,12.9957886 L4,11.9957886 L4,8.00000001 L4,8 C4,5.57933155 5.72018994,3.56043498 8.00484244,3.09903766 C8.00163949,3.06645872 8,3.03342082 8,3 C8,2.44771525 8.44771525,2 9,2 C9.55228475,2 10,2.44771525 10,3 C10,3.03342082 9.99836051,3.06645872 9.99515756,3.09903766 Z"></path> </g> </svg>
+                    <NotificationsIcon />
                   </button>
                   <button className="p6n-platformbar-utility p6n-utilities-menu-icon md-icon-button md-primary md-button md-platform-bar-theme md-ink-ripple">
-                    <svg id="settings" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" fit="" height="100%" width="100%" preserveAspectRatio="xMidYMid meet" focusable="false"> <g> <path d="M8.5,5 C7.67125,5 7,4.32875 7,3.5 C7,2.67125 7.67125,2 8.5,2 C9.32875,2 10,2.67125 10,3.5 C10,4.32875 9.32875,5 8.5,5 Z M8.5,10 C7.67125,10 7,9.32875 7,8.5 C7,7.67125 7.67125,7 8.5,7 C9.32875,7 10,7.67125 10,8.5 C10,9.32875 9.32875,10 8.5,10 Z M8.5,15 C7.67125,15 7,14.32875 7,13.5 C7,12.67125 7.67125,12 8.5,12 C9.32875,12 10,12.67125 10,13.5 C10,14.32875 9.32875,15 8.5,15 Z"></path> </g> </svg>
+                    <SettingsIcon />
                   </button>
                 </div>
                 <div id="pcc-account-chooser-container">
                   <div className="cfc-accountchooser-dynamic">
                     <button className="cfc-accountchooser-link">
-                      <img alt="user" src="https://lh6.googleusercontent.com/-hk3W3L-0IpA/AAAAAAAAAAI/AAAAAAAAAAA/5teywp-SKt4/W96-H96/photo.jpg" />
+                      <img alt="user" src={userPhoto} />
                     </button>
                   </div>
                 </div>
